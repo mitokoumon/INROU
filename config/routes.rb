ed7471faace1973ddc9f1207ec584f7613be9077
vagrant <kameitatsuki0624@gmail.com>
2020-05-15 09:22:13 +0000

    routing change

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
-  resource :users only:[:show, :edit, :update] do
-    resources :receivers only:[:index, :show, :create, :destroy, :edit]
-    resources :carts only:[:index, :create, :destroy]
-    resources :orders only:[:index, :show, :new, :create]
+  resource :users,only:[:show, :edit, :update] do
+    resources :receivers,only:[:index, :show, :create, :destroy, :edit]
+    resources :carts,only:[:index, :create, :destroy]
+    resources :orders,only:[:index, :show, :new, :create]
     get '/orders/thanks' => 'orders#thanks'
     get '/orders/check' => 'orders#check'
     delete '/carts' => 'carts#destroy_all'

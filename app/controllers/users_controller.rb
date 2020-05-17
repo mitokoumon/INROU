bb3cd7633bd87a3136e44b3b68fa5680f08d8dc7
tatsukikamei <61717067+tatsuki-kamei@users.noreply.github.com>
2020-05-17 11:44:14 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-	before_action :taikai
 	before_action :authenticate_user!
 	def show
 		@user = User.find(current_user.id)
@@ -12,7 +11,12 @@ class UsersController < ApplicationController
 	def update
 		@user = User.find(current_user.id)
 		@user.update(user_params)
-	  	redirect_to users_path
+		if @user.flag == 1 then
+	  		redirect_to users_path
+		elsif @user.flag == 2
+			reset_session
+      			redirect_to root_path
+		end
 	end
 
 	def flag
@@ -22,14 +26,6 @@ class UsersController < ApplicationController
 	private
 	def user_params
 		params.require(:user).permit(:family_name, :last_name, :family_name_kana, :last_name_kana, :post_code, :address, :telephone_number, :email, :flag)
-	end
-
-	def taikai
-		@user = current_user
-		if @user.flag == 2 then
-			reset_session
-			redirect_to root_path
-		end
 	end
 
 end

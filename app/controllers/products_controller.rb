03b714e7c086be14b6d37d98d4830113c74345d2
vagrant <vagrant@localhost.localdomain>
2020-05-19 07:44:41 +0000

    Add cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,21 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.where(flag: 1)
   	@categories = Category.where(flag: 1)
+    @products = Product.all
   end
 
   def show
   	@product = Product.find(params[:id])
   	@categories = Category.where(flag: 1)
   	@cart = Cart.new
-    if @product.category.flag == 2 || @product.flag == 2
+    if @product.category.flag == 2
       redirect_to products_path
     end
   end
 
   def judge
   	@category = Category.find(params[:id])
-  	products = @category.products
-    @products = products.where(flag: 1)
+  	@products = @category.products
   	@categories = Category.where(flag: 1)
   	render :index
   end

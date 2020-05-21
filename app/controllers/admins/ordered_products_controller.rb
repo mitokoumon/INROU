6b4b77afcf6a3d1bb2a53bdaa92190b2b480cdb7
Tatsuro <tatsurow1122@gmail.com>
2020-05-21 10:39:57 +0900

    Update

--- app/controllers/admins/ordered_products_controller.rb
+++ app/controllers/admins/ordered_products_controller.rb
@@ -11,10 +11,11 @@ class Admins::OrderedProductsController < ApplicationController
 		ordered_product.update(ordered_products_params)
 		order = ordered_product.order
 		ordered_products = OrderedProduct.where(order_id: order.id)
+		ordered_products_flags = OrderedProduct.where(order_id: order.id, flag: 4)
 		if ordered_product.flag == 3
 			order.update(flag: 3)
   		redirect_to request.referer
-		elsif ordered_products.flag == 4
+		elsif ordered_products.count == ordered_products_flags.count
 			order.update(flag: 4)
 			redirect_to request.referer
 		else

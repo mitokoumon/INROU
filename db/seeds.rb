c3a24c3cbe9282780857a7cd39624e790dc7ec19
ryusuke06 <62837064+ryusuke06@users.noreply.github.com>
2020-05-21 10:45:22 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
+
 ordered_product = OrderedProduct.new(:order_id=>"1",:product_id=>"1",:number=>"3",:flag=>"1",:price=>"1")
 ordered_product.save!
 
@@ -14,14 +14,3 @@ ordered_product.save!
 
 #order = Order.new(:user_id =>"1",:post_code =>"1",:address =>"1",:name =>"1",:total_price  =>"1", :shipping_fee  =>"1",:payment  =>"1",:flag =>"1")
 #order.save!
-=======
-#admin = Admin.new(:email => 'hoge@hoge.com', :password => 'mitokoumon')
-#admin.save!
-
-#order = Order.new(:user_id =>"1",:post_code =>"1",:address =>"1",:name =>"1",:total_price  =>"1", :shipping_fee  =>"1",:payment  =>"1",:flag =>"1")
-#order.save!
-
-ordered_product = OrderedProduct.new(:order_id=>"1",:product_id=>"1",:number=>"3",:flag=>"1",:price=>"1")
-ordered_product.save!
-
->>>>>>> d2a5f6f42ce1ff67e692806217086edab3e895c9

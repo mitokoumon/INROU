e6b922dfde56ba99feb2ebe855e9ae1b276ee221
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-05-21 15:35:30 +0900

    comment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,14 +13,18 @@ class OrdersController < ApplicationController
   def check
 	@order = Order.new
 	@order.ordered_products.build
+  # orderとordered_productに同時に保存する際に必要
 	@payment = params[:payment]
+  # new画面のpaymentのデータを取り出す
 	zyusyo = params[:zyusyo]
+  # new画面のzyusyo(ラジオボタンの番号)のデータを取り出す
 	@total_price = 0
 	current_user.carts.each do |cart|
 		@total_price += cart.product.now_price * cart.number * 1.1
 	end
   	if zyusyo == "1"
   		@aaa = current_user.address_all
+      # user.rbに定義したメソッド(address_all)を使用
   		@ddd = current_user.address
   		@bbb = current_user.post_code
   		@ccc = current_user.family_name+current_user.last_name
@@ -28,6 +32,7 @@ class OrdersController < ApplicationController
   		reciever_id = params[:zyusyo2]
   		receiver = Receiver.find(reciever_id)
   		@aaa = receiver.address_all
+      # receiver.rbに定義したメソッド(address_all)を使用
   		@ddd = receiver.address
   		@bbb = receiver.post_code
   		@ccc = receiver.name
@@ -38,6 +43,7 @@ class OrdersController < ApplicationController
   		@ccc = params[:name]
   	else
   	end
+    # 選んだラジオボタンによってユーザーの住所、登録配送先、入力した住所を取り出せるようにする
   end
 
   def thanks
@@ -55,4 +61,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:user_id,:post_code, :address, :name, :total_price, :shipping_fee, :payment, :flag, ordered_products_attributes:[:id, :product_id, :number, :flag, :price])
   end
+  # order.rbへaccepts_nested_attributes_for :ordered_products　、ordered_products_attributes:[:id, :product_id, :number, :flag, :price]することでordered_productのparamsとして
+  # 取得できるようにする。
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :ordered_products, dependent: :destroy
   accepts_nested_attributes_for :ordered_products
+  # orderのデータと同時にordered_productsのデータを保存する際に必要
 end

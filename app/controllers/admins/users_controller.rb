4268b9fa209afc31b619ed8aab32c8f319419b6b
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-05-25 11:22:43 +0900

    fix app_controller,Add searchfunction

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,7 +1,11 @@
 class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
 	def index
-		@users = User.page(params[:page]).per(10)
+		@users = User.page(params[:user_page]).per(10)
+		respond_to do |format|
+    	format.html
+    	format.js
+  	end
 	end
 
 	def show
@@ -24,20 +28,32 @@ class Admins::UsersController < ApplicationController
 	def search
 		split_keyword = params[:search].split(/[[:blank:]]+/)
 		# 検索窓に入力した文字列を空白で分割する
-	  @users = []
-	  @products = []
+	  users = []
+	  products = []
 	  # 各キーワードで調べた結果を入れるための箱を作る
 	  split_keyword.each do |keyword|
 	    next if keyword == ""
-	    @users += User.where('family_name LIKE ?', "%#{keyword}%").or(User.where(['last_name LIKE ?', "%#{keyword}%"])).or(User.where(['family_name_kana LIKE ?', "%#{keyword}%"])).or(User.where(['last_name_kana LIKE ?', "%#{keyword}%"]))
-	    @products += Product.where('name LIKE ?', "%#{keyword}%")
+	    users += User.where('family_name LIKE ?', "%#{keyword}%").or(User.where(['last_name LIKE ?', "%#{keyword}%"])).or(User.where(['family_name_kana LIKE ?', "%#{keyword}%"])).or(User.where(['last_name_kana LIKE ?', "%#{keyword}%"]))
+	    products += Product.where('name LIKE ?', "%#{keyword}%")
 	  end
 	  # 各キーワード毎に検索して、[]に入れる
-	  @users.uniq!
-	  @products.uniq!
+	  users.uniq!
+	  products.uniq!
 	  # []の中で重複した情報を削除する
 		# @users = User.search(params[:search])
 		# @products = Product.search(params[:search])
+		user1 = []
+		product1 = []
+		users.each do |user|
+			user1.append(user)
+		end
+		products.each do |product|
+			if product.category.flag == 1
+				product1.append(product)
+			end
+		end
+		@users = Kaminari.paginate_array(user1).page(params[:user_page]).per(5)
+		@products = Kaminari.paginate_array(product1).page(params[:product_page]).per(5)
 	end
 
 	private

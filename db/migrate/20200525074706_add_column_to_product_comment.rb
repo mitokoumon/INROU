d8812f11b2ed4755744923bbf5f89afba8ac76ce
ryusuke06 <or.06.012@gmail.com>
2020-05-26 07:39:45 +0000

    fixed migration file

--- db/migrate/20200525074706_add_column_to_product_comment.rb
+++ db/migrate/20200525074706_add_column_to_product_comment.rb
@@ -1,5 +1,6 @@
 class AddColumnToProductComment < ActiveRecord::Migration[5.2]
   def change
     add_column :product_comments, :title, :string
+    add_column :product_comments, :rate, :float
   end
 end
